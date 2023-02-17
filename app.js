39c0f8147ed568f82cd61b6dba56dba6eaa4b064
umeshgrg29 <umeshgrg29@gmail.com>
2023-02-17 17:49:16 +0530

    Creating different routes for the app using http module only

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('My name is umesh garg')
+    const url = req.url;
+    if(url === '/home'){
+        res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html><head><title>My app</title></head><body><h1>Welcome Home </h1></body></html>')
+    return res.end()
+    }
+    else if(url === '/about'){
+        res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html><head><title>My app</title></head><body><h1>Welcome to about us Page </h1></body></html>')
+    return res.end()
+    }
+    else if(url === '/node'){
+        res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html><head><title>My app</title></head><body><h1>Welcome to my node js project </h1></body></html>')
+    return res.end()
+    }
+
+    
 }
 )
 
