a2cb26b62137862f49b8f4fa7220ace37bde233d
umeshgrg29 <umeshgrg29@gmail.com>
2023-02-20 11:43:13 +0530

    Starting with express

--- app1.js
+++ app1.js
@@ -1,5 +1,19 @@
-const http = require('http')
-const routes = require('./routes')
+// const http = require('http')
+// const routes = require('./routes')
 
-const server = http.createServer(routes)
-server.listen(4000);
+const express = require('express')
+const app = express();
+
+app.use((req, res, next)=>{
+    console.log('In the middleware')
+    next(); // allow the request to continue to the next middleware in line
+})
+
+app.use((req, res, next)=>{
+    console.log('In another middleware')
+    res.send('<h1>Hello from Express!</h1>')
+
+})
+
+
+app.listen(4000)
