74c4b2a54b70f59544baf5918940a41d00f96efc
umeshgrg29 <umeshgrg29@gmail.com>
2023-02-22 07:51:09 +0530

    using middlewaeres

--- app1.js
+++ app1.js
@@ -3,15 +3,23 @@
 
 const express = require('express')
 const app = express();
+const bodyParser = require('body-parser')
 
-app.use((req, res, next)=>{
-    console.log('In the middleware')
-    next(); // allow the request to continue to the next middleware in line
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/add-product', (req, res, next)=>{
+    // console.log('In the middleware')
+    res.send(`<form action = "/product" method = "POST"><input type="text" name = "title" ><input type="text" name = "size" ><button type="submit">Add</button></form>`)
 })
 
-app.use((req, res, next)=>{
-    console.log('In another middleware')
-    res.send('<h1>Hello from Express!</h1>')
+app.post('/product', (req, res, next) =>{
+    console.log(req.body)
+    res.redirect('/');
+})
+
+app.use('/', (req, res, next)=>{
+    // console.log('In another middleware')
+    res.send(`<h1>In the express js home page</h1>`)
 
 })
 
